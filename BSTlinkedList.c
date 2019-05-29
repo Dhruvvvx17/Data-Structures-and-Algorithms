0e54dac62daef62f247d3d808b6ed21936589a02
Dhruv Vohra <dhruv.v405@gmail.com>
2019-05-29 10:23:09 +0530

    Updated BST

--- BSTlinkedList.c
+++ BSTlinkedList.c
@@ -6,44 +6,53 @@ typedef struct node{
     struct node *left, *right;
 }node;
 
-
-node* createBST();
+void createBST();
+void preorder(node *r); 
 void inorder(node *r);
+void postorder(node *r);
+node *delete(node *r, int ele);
+void clear(node *r);
 
-node* createBST(){
-    node *temp, *r, *p, *q;
-    int wish;
-    r = (node*)malloc(sizeof(node));
-    printf("\nEnter the root element: ");
-    scanf("%d",&r->data);
-    r->left = NULL;
-    r->right = NULL;
-    do{
-        temp = (node*)malloc(sizeof(node));
-        printf("Enter element: ");
-        scanf("%d",&temp->data);
-        temp->left = NULL;
-        temp->right = NULL;
+void createBST(node *r){
+    node *temp, *p, *q;
+    temp = (node*)malloc(sizeof(node));
+    printf("Enter element: ");
+    scanf("%d",&temp->data);
+    temp->left = NULL;
+    temp->right = NULL;
 
-        q = NULL;
-        p = r;
-        if (p->data == temp->data){
-            printf("Duplicate\n");
-            free(temp);
-            break;
-        }
-        while(p!=NULL && p->data != temp->data){
-            q = p;
-            if(temp->data < p->data)    p = p->left;
-            else    p = p->right;
-        }
-        if(temp->data < q->data)    q->left = temp;
-        else    q->right = temp;
+    q = NULL;
+    p = r;
+    if (p->data == temp->data){
+        printf("Element already inserted.\n");
+        free(temp);
+        return;
+    }
+    //travers to the new element's parent
+    while(p!=NULL && p->data != temp->data){
+        q = p;
+        if(temp->data < p->data)
+            p = p->left;
+        else
+            p = p->right;
+    }
+    //insertion
+    if(temp->data < q->data)
+        q->left = temp;
+    else
+        q->right = temp;
 
-        printf("Add another node? (1/0): ");
-        scanf("%d",&wish);
-    }while(wish);
-    return(r);
+    printf("Element %d inserted successfully\n",temp->data);
+    return;
+}
+
+void preorder(node *r){
+    if(r!= NULL){
+        printf("%d ",r->data);
+        preorder(r->right);
+        preorder(r->left);
+    }
+    return;    
 }
 
 void inorder(node *r){
@@ -55,9 +64,111 @@ void inorder(node *r){
     return;
 }
 
+void postorder(node *r){
+    if(r!= NULL){
+        postorder(r->right);
+        postorder(r->left);
+        printf("%d ", r->data);
+    }
+    return;    
+}
+
+node* delete(node *r,int ele){
+    node *temp , *p;
+    if (r == NULL)  return r;
+    
+    if(ele < r->data)
+        r->left = delete(r->left, ele);
+    else if(ele > r->data)
+        r->right = delete(r->right, ele);
+    else{
+        if(r->left == NULL){
+            temp = r->right;
+            free(r);
+            return temp;
+        }
+        else if(r->right == NULL){
+            temp = r->left;
+            free(r);
+            return temp;
+        }
+        else{
+            p = r->right;
+            while(p->left != NULL){
+                p = p->left;
+            }
+            r->data = p->data;
+            r->right = delete(r->right,p->data);
+            return r;
+        }
+    }
+    return r;
+}
+
+void clear(node *r){
+    if(r->left != NULL){
+        clear(r->left);
+    }
+    if(r->left != NULL){
+        clear(r->right);
+    }
+    free(r);
+    return;
+}
+
 void main()
 {
-    node *root = NULL;
-    root = createBST();
-    inorder(root);
+    node *root;
+    root = (node *)malloc(sizeof(node));
+    printf("\n\t------------------\n\tBINARY SEARCH TREE\n\t------------------\n");
+    printf("\nEnter the root element: ");
+    scanf("%d", &root->data);
+    root->left = NULL;
+    root->right = NULL;
+    int choice,ele,loop=1;
+    while(loop){
+        printf("\n----------------------------\n1. Insert\n2. Delete\n3. Inorder traversal\n4. Preorder traversal\n5. Postorder traversal\n6. Exit\n");
+        printf("\nSelect an option: ");
+        scanf("%d",&choice);
+        printf("\n");
+        switch(choice){
+            case 1://insert
+                createBST(root);
+                break;
+
+            case 2://delete (does not handle the case of ele not present)
+                printf("Enter element: ");
+                scanf("%d",&ele);
+                root = delete(root,ele);
+                printf("Element deleted successfully\n");
+                break;
+
+            case 3://inorder
+                printf("Inorder: ");
+                inorder(root);
+                printf("\n");
+                break;
+
+            case 4://preorder
+                printf("Preorder: ");
+                preorder(root);
+                printf("\n");
+                break;
+
+            case 5://postorder
+                printf("Postorder: ");
+                postorder(root);
+                printf("\n");
+                break;
+
+            case 6://exit
+                clear(root);
+                loop = 0;
+                printf("--------------EXIT--------------\n");
+                break;
+
+            default:
+                break;
+        }
+    }
 }
